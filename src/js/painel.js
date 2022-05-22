8c6791c763da818d9ef4fed87bf0effe5002807b
Abreeu <98714169+Abreeu@users.noreply.github.com>
2022-05-22 16:30:01 -0300

    Update painel.js

--- src/js/painel.js
+++ src/js/painel.js
@@ -1,17 +1,3 @@
-/*
-    quando clicar na seta pra avançar temos que 
-    esconder todas as imagens e mostrar a próxima imagem
-        a imagem atual começa em 0 pq é a primeira imagem 
-        assim que for clicado no avançar eu preciso adicionar mais 1 ao contador de imagens
-        pra poder saber que agora eu vou mostrar a segunda imagem
-        esconder todas as imagens
-            pegar todas as imagens usando o DOM e remover a classe mostrar 
-        
-        mostrar a próxima imagem 
-            pegar todas as imagens, descobrir qual é a próxima, e colocar a classe 
-            mostrar nela
-*/
-
 const imagensPainel = document.querySelectorAll('.imagem-painel');
 const setaAvancar = document.getElementById('btn-avancar');
 const setaVoltar = document.getElementById('btn-voltar');
